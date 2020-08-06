ba83494bca15e3bd4bb4f08e33e81c2bcbb08282
Ben <benjisgarza@gmail.com>
2020-08-05 22:20:02 -0700

    Removing unneeded white space

--- script.js
+++ script.js
@@ -5,12 +5,9 @@
 const ticTacToeGame = new TicTacToeGame();
 ticTacToeGame.start();
 
-
-
 // constractor functions to curry all the game's functionalities.
 // later, we will find a way to refactor this to use factory fubctions.
 function TicTacToeGame(){
-
 	const board = new Board();
 	const human = new Human(board);
 	const computer = new Computer(board);
@@ -19,46 +16,32 @@ function TicTacToeGame(){
 	this.start = function(){
 		const config = { childList: true};
 		const observer = new MutationObserver(() => takeTurn());
-
 		//observing/watching board positions at the start of the game.
 		board.positions.forEach((el) => observer.observe(el, config));
 		takeTurn();    
 	}
 
 	//function to controls turns between the players
-
 	function takeTurn() {
-
 		//stopping players to take another turn when the game is over.
 		if (board.checkWinner()) {
 			return;  
 		}
-
 		if (turn % 2 === 0) {
 			human.takeTurn();
 		} else {
 			computer.takeTurn();
 		}
-
 	turn++;
-
 	}
 }
-
-	
-
-
-
 
 function Board(){
   	// tracking board positions
   this.positions = Array.from(document.querySelectorAll('.grid-item'));
-
   // checking for winner
   this.checkWinner = function() {
-
   	 let winner = false;
-
   	 const winningCombinations = [
 							        [0,1,2],
 							        [3,4,5],
@@ -69,9 +52,8 @@ function Board(){
 							        [1,4,7],
 							        [2,5,8]
     										];
-
-      const positions = this.positions;
-
+			const positions = this.positions;
+			
       // checking for winning combinations
 	  winningCombinations.forEach((winningCombo) => {
 	  const pos0InnerText = positions[winningCombo[0]].innerText;
@@ -96,7 +78,6 @@ function Board(){
 function Human(board){
   this.takeTurn = function(){
    board.positions.forEach(el => el.addEventListener('click', handleTurnTaken));
-
   }
 
   function handleTurnTaken(event){
@@ -107,7 +88,6 @@ function Human(board){
 
 function Computer(board){
 	this.takeTurn = function() {
-
 		// checking available positions after picking a spot
 		const positionsAvailable = board.positions.filter((p) => p.innerText === '' );
 		console.log(positionsAvailable)
